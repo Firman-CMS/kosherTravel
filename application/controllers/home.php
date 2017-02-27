0a959efcd9212ec1cf22a97a1a160bbc53c83729
fcms_dev <firman.766hi@gmail.com>
2017-02-27 18:35:18 +0700

    update front

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -8,30 +8,26 @@ class Home extends CI_Controller {
 
 	function __construct() {
 		parent::__construct();
+      $this->load->model(array('m_message'));
 		// $this->load->model(array('m_login'));
 		// ini_set('display_errors', 1);
 	}
 
-	public function index()
+   public function index()
 	{
-		// $data['menu'] 				= 'Dashboard';
-		// $data['title'] 			= '';
-      // ta['notif_message'] 	= $this->countNotifMessage();
-		$data['content'] 			= $this->load->view('front/home/v_home');
-		// $this->load->view('v_base', $data);
+      $data['menu'] 		= 'Dashboard';
+		$maincontent['content']  = $this->load->view('front/home/v_home', $data, true);
+		$this->load->view('front/v_base',$maincontent );
 	}
 
-	function countNotifMessage(){
-		$this->load->model('m_message', "", TRUE);
-		$data = $this->m_message->countNotif();
-		return $data;
-	}
+   public function newmessage(){
+		$data['subject']	= $this->input->post('subject');
+		$data['email'] 	= $this->input->post('email');
+		$data['from'] 		= $this->input->post('from');
+		$data['phone'] 	= $this->input->post('phone');
+		$data['body'] 		= $this->input->post('body');
 
-	// public function index(){
-	// 	ini_set('display_errors', 1);
-	// 	$data['title'] ='New User';
-	// 	$data['content'] = $this->load->view('user/v_new_user' , $data, true);
-	// 	$this->load->view('v_base', $data);
-	// }
+		$this->m_message->save($data);
+	}
 
 }
